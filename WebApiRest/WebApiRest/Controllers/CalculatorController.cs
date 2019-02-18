2db7a59dbfbfddd834fe873f41ff5d6ab44829f9
LucasCS <lucascatalanosouza@hotmail.com>
2019-02-18 11:29:58 -0300

    calculator OK

---
+++ WebApiRest/WebApiRest/Controllers/CalculatorController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        
+        // GET api/calculator/soma/5/5
+        [HttpGet("soma/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Soma(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/calculator/sub/5/5
+        [HttpGet("sub/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sub(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/calculator/div/5/5
+        [HttpGet("div/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Div(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/calculator/mult/5/5
+        [HttpGet("mult/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mult(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        //metodos
+        private decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string strNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumber;
+        }
+    }
+}
